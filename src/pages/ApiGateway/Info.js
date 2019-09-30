c4ee3a22f378c9d2db44d04c836a5e39bd6d91f8
Acoco <490145346@qq.com>
2019-09-30 11:30:42 +0800

    api列表样式修改 whd  2019-9-30 whd

--- src/pages/ApiGateway/Info.js
+++ src/pages/ApiGateway/Info.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'dva';
+import React, {Component} from 'react';
+import {connect} from 'dva';
 import router from 'umi/router';
-import { FormattedMessage } from 'umi-plugin-react/locale';
-import { Menu } from 'antd';
+import {FormattedMessage} from 'umi-plugin-react/locale';
+import {Menu} from 'antd';
 import GridContent from '@/components/PageHeaderWrapper/GridContent';
 import styles from './Info.less';
 
-const { Item } = Menu;
+const {Item} = Menu;
 
-@connect(({ user}) => ({
+@connect(({user}) => ({
   currentUser: user.currentUser
 }))
 class Info extends Component {
   constructor(props) {
     super(props);
-    const { match, location } = props;
+    const {match, location} = props;
     const menuMap = {
       list: <FormattedMessage id="app.settings.menuMap.apiList" defaultMessage="Own API List" />,
       callList: (
@@ -23,73 +23,44 @@ class Info extends Component {
     };
     const key = location.pathname.replace(`${match.path}/`, '');
     this.state = {
-      mode: 'inline',
       menuMap,
       selectKey: menuMap[key] ? key : 'list',
     };
   }
 
   static getDerivedStateFromProps(props, state) {
-    const { match, location } = props;
+    const {match, location} = props;
     let selectKey = location.pathname.replace(`${match.path}/`, '');
     selectKey = state.menuMap[selectKey] ? selectKey : 'base';
     if (selectKey !== state.selectKey) {
-      return { selectKey };
+      return {selectKey};
     }
     return null;
   }
 
-  componentDidMount() {
-    window.addEventListener('resize', this.resize);
-    this.resize();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.resize);
-  }
-
   getmenu = () => {
-    const { menuMap } = this.state;
+    const {menuMap} = this.state;
     return Object.keys(menuMap).map(item => <Item key={item}>{menuMap[item]}</Item>);
   };
 
   getRightTitle = () => {
-    const { selectKey, menuMap } = this.state;
+    const {selectKey, menuMap} = this.state;
     return menuMap[selectKey];
   };
 
-  selectKey = ({ key }) => {
+  selectKey = ({key}) => {
     router.push(`/apiGateway/apiList/${key}`);
     this.setState({
       selectKey: key,
     });
   };
 
-  resize = () => {
-    if (!this.main) {
-      return;
-    }
-    requestAnimationFrame(() => {
-      let mode = 'inline';
-      const { offsetWidth } = this.main;
-      if (this.main.offsetWidth < 641 && offsetWidth > 400) {
-        mode = 'horizontal';
-      }
-      if (window.innerWidth < 768 && offsetWidth > 400) {
-        mode = 'horizontal';
-      }
-      this.setState({
-        mode,
-      });
-    });
-  };
-
   render() {
-    const { children, currentUser } = this.props;
+    const {children, currentUser} = this.props;
     if (!currentUser.userid) {
       return '';
     }
-    const { mode, selectKey } = this.state;
+    const {selectKey} = this.state;
     return (
       <GridContent>
         <div
@@ -98,13 +69,10 @@ class Info extends Component {
             this.main = ref;
           }}
         >
-          <div className={styles.leftmenu}>
-            <Menu mode={mode} selectedKeys={[selectKey]} onClick={this.selectKey}>
+          <div className={styles.right}>
+            <Menu mode="horizontal" selectedKeys={[selectKey]} onClick={this.selectKey} style={{width: '100%'}}>
               {this.getmenu()}
             </Menu>
-          </div>
-          <div className={styles.right}>
-            <div className={styles.title}>{this.getRightTitle()}</div>
             {children}
           </div>
         </div>
